025a8cd47f2208cc870f80409b722da0c1c994e5
Anna <aksmithnw@gmail.com>
2024-02-14 00:42:22 -0800

    Implement playRound

--- game-script.js
+++ game-script.js
@@ -15,7 +15,24 @@ function playGame() {
  * @returns result message
  */
 function playRound(playerSelection, computerSelection) {
-    // TODO
+    let playerSelLower = playerSelection.toLowerCase();
+    let computerSelLower = computerSelection.toLowerCase();
+
+    if (playerSelLower === computerSelLower) {
+        return "Tie!";
+    }
+    if (beats(playerSelLower, computerSelLower)) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } 
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+}
+
+function beats(selection1, selection2) {
+    let beat = (selection1 == "rock" && selection2 == "scissors");
+    beat |= (selection1 == "paper" && selection2 == "rock");
+    beat |= (selection1 == "scissors" && selection2 == "paper");
+
+    return beat;
 }
 
 /**
