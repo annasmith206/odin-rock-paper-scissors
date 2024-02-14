95628d04927893f8f87cb4c5b5c93fa7a9998c80
Anna <aksmithnw@gmail.com>
2024-02-13 16:17:16 -0800

    Initialize files, define & document functions
    Create empty HTML and game script with unimplemented playGame,
    playRound, and getComputerChoice functions

---
+++ game-script.js
@@ -0,0 +1,27 @@
+playGame();
+
+/**
+ * Play five rounds, report winner/loser at the end.
+ */
+function playGame() {
+    // TODO
+    console.log("Welcome to Rock Paper Scissors");
+}
+
+/**
+ * Plays single round of rock paper scissors
+ * @param {*} playerSelection player's choice
+ * @param {*} computerSelection computer's choice
+ * @returns result message
+ */
+function playRound(playerSelection, computerSelection) {
+    // TODO
+}
+
+/**
+ * Randomly return computer's choice 
+ * @returns 'Rock', 'Paper', or 'Scissors'
+ */
+function getComputerChoice() {
+    // TODO
+}
