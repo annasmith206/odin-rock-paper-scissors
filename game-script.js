005a6d37d6d0b972312aafc92a1cd5b5a6a2debd
Anna <aksmithnw@gmail.com>
2024-02-13 16:22:42 -0800

    Implement getComputerChoice
    Implement getComputerChoice to randomly return Rock, Paper, or
    Scissors.

--- game-script.js
+++ game-script.js
@@ -23,5 +23,14 @@ function playRound(playerSelection, computerSelection) {
  * @returns 'Rock', 'Paper', or 'Scissors'
  */
 function getComputerChoice() {
-    // TODO
+    choice = Math.floor(Math.random() * 3);
+
+    switch (choice) {
+        case 0:
+            return "Rock"
+        case 1:
+            return "Paper"
+        case 2:
+            return "Scissors"
+    }
 }
