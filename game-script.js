56c0ca5f949bf394cf32ef0b1b111b43de7e9c6e
Anna <aksmithnw@gmail.com>
2024-02-14 11:26:05 -0800

    Add comments, clean up functions
    Rearanged functions to be more modular, added documentation above
    function headers

--- game-script.js
+++ game-script.js
@@ -8,33 +8,34 @@ function playGame() {
     let computerScore = 0; 
 
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt(`Game ${i}/5. Enter Selection: `)
-        let computerSelection = getComputerChoice()
-        let result = playRound(playerSelection, computerSelection)
+        let result = playRound(i);
+
         if (result == "win") {
             playerScore ++;
         } else if (result == "lose") {
             computerScore ++;
         }
     }
-
-    if (playerScore > computerScore) {
-        alert(`Congratulations! You've won! Your score: ${playerScore}. Computer score: ${computerScore}`)
-    } else if (computerScore > playerScore) {
-        alert(`Sorry. You lost. Your score: ${playerScore}. Computer score: ${computerScore}`)
-    } else {
-        alert(`It's a tie! Your score: ${playerScore}. Computer score: ${computerScore}`)
-    }
-
+    alertResults(playerScore, computerScore);
 }
 
 /**
- * Plays single round of rock paper scissors, prints result message
+ * Play a single round of rock paper scissors. Return result of round
+ * @returns "tie", "win", or "lose"
+ */
+function playRound(roundNumber){
+    let playerSelection = prompt(`Game ${roundNumber}/5. Enter Selection: `);
+    let computerSelection = getComputerChoice();
+    return judgeSelections(playerSelection, computerSelection);
+}
+
+/**
+ * Judges selections, prints result message
  * @param {*} playerSelection player's choice
  * @param {*} computerSelection computer's choice
- * @returns result
+ * @returns "tie", "win", or "lose"
  */
-function playRound(playerSelection, computerSelection) {
+function judgeSelections(playerSelection, computerSelection) {
     let playerSelLower = (playerSelection ?? "").toLowerCase();
     let computerSelLower = computerSelection.toLowerCase();
 
@@ -50,6 +51,12 @@ function playRound(playerSelection, computerSelection) {
     return "lose";
 }
 
+/**
+ * Determines whether selection 1 beat selection 2
+ * @param {*} selection1 
+ * @param {*} selection2 
+ * @returns True if selection 1 wins, False otherwise
+ */
 function beats(selection1, selection2) {
     let beat = (selection1 == "rock" && selection2 == "scissors");
     beat |= (selection1 == "paper" && selection2 == "rock");
@@ -72,5 +79,20 @@ function getComputerChoice() {
             return "Paper"
         case 2:
             return "Scissors"
+    }
+}
+
+/**
+ * Alerts final results of game
+ * @param {*} playerScore Player's total
+ * @param {*} computerScore Computer's total wins
+ */
+function alertResults(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        alert(`Congratulations! You've won! Your score: ${playerScore}. Computer score: ${computerScore}`)
+    } else if (computerScore > playerScore) {
+        alert(`Sorry. You lost. Your score: ${playerScore}. Computer score: ${computerScore}`)
+    } else {
+        alert(`It's a tie! Your score: ${playerScore}. Computer score: ${computerScore}`)
     }
 }
